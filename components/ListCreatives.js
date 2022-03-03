5ca4b50ca9704d15159ea750da45ca728f963b99
Numa Maurin <numa.maurin@teads.tv>
2022-03-03 10:27:47 +0100

    concat list creatives

--- components/ListCreatives.js
+++ components/ListCreatives.js
@@ -1,71 +1,72 @@
-import React from 'react'
-import { Text, View, Pressable } from 'react-native'
-import ModalCustom from '../components/ModalCustom'
-import styles from '../styles/styles'
+import React from "react";
+import { Text, View, Pressable } from "react-native";
+import ModalCustom from "../components/ModalCustom";
+import styles from "../styles/styles";
 
-const ListCreatives = ({ pid, onChangePid, modalVisible, setModalVisible, selectedCreative, setSelectedCreative }) => {
+const ListCreatives = ({
+  pid,
+  onChangePid,
+  modalVisible,
+  setModalVisible,
+  selectedCreative,
+  setSelectedCreative,
+}) => {
   const CreativesEnum = {
-    landscape: 'Landscape',
-    vertical: 'Vertical',
-    square: 'Square',
-    caroussel: 'Caroussel',
-    custom: 'Custom'
-  }
+    custom: "Custom",
+  };
+  const creativeTypes = ["Landscape", "Vertical", "Square", "Caroussel"];
+
+  const list = () => {
+    return creativeTypes.map((creativeType) => {
+      return (
+        <View key={creativeType}>
+          <Pressable
+            style={
+              selectedCreative == creativeType
+                ? styles.button
+                : styles.buttonUnpressed
+            }
+            onPress={() => setSelectedCreative(creativeType)}
+          >
+            <Text
+              style={
+                selectedCreative == creativeType
+                  ? styles.text
+                  : styles.textUnpressed
+              }
+            >
+              {creativeType}
+            </Text>
+          </Pressable>
+        </View>
+      );
+    });
+  };
+
   return (
     <View>
       <Text style={styles.listText}>Creatives</Text>
-      <View style={styles.containerRow}>
-        <View>
-          <Pressable
-            style={selectedCreative == CreativesEnum.landscape ? styles.button : styles.buttonUnpressed}
-            onPress={() => setSelectedCreative(CreativesEnum.landscape)}
-          >
-            <Text style={selectedCreative == CreativesEnum.landscape ? styles.text : styles.textUnpressed}>
-              {CreativesEnum.landscape}
-            </Text>
-          </Pressable>
-        </View>
+      <View style={styles.containerRow}>{list()}</View>
 
-        <View>
-          <Pressable
-            style={selectedCreative == CreativesEnum.vertical ? styles.button : styles.buttonUnpressed}
-            onPress={() => setSelectedCreative(CreativesEnum.vertical)}
-          >
-            <Text style={selectedCreative == CreativesEnum.vertical ? styles.text : styles.textUnpressed}>
-              {CreativesEnum.vertical}
-            </Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable
-            style={selectedCreative == CreativesEnum.square ? styles.button : styles.buttonUnpressed}
-            onPress={() => setSelectedCreative(CreativesEnum.square)}
-          >
-            <Text style={selectedCreative == CreativesEnum.square ? styles.text : styles.textUnpressed}>
-              {CreativesEnum.square}
-            </Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable
-            style={selectedCreative == CreativesEnum.caroussel ? styles.button : styles.buttonUnpressed}
-            onPress={() => setSelectedCreative(CreativesEnum.caroussel)}
-          >
-            <Text style={selectedCreative == CreativesEnum.caroussel ? styles.text : styles.textUnpressed}>
-              {CreativesEnum.caroussel}
-            </Text>
-          </Pressable>
-        </View>
-      </View>
       <View style={styles.containerRow}>
-        <View style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
           <Pressable
-            style={selectedCreative == CreativesEnum.custom ? styles.button : styles.buttonUnpressed}
+            style={
+              selectedCreative == CreativesEnum.custom
+                ? styles.button
+                : styles.buttonUnpressed
+            }
             onPress={() => {
-              setSelectedCreative(CreativesEnum.custom), setModalVisible(true)
+              setSelectedCreative(CreativesEnum.custom), setModalVisible(true);
             }}
           >
-            <Text style={selectedCreative == CreativesEnum.custom ? styles.text : styles.textUnpressed}>
+            <Text
+              style={
+                selectedCreative == CreativesEnum.custom
+                  ? styles.text
+                  : styles.textUnpressed
+              }
+            >
               {CreativesEnum.custom}
             </Text>
           </Pressable>
@@ -78,7 +79,7 @@ const ListCreatives = ({ pid, onChangePid, modalVisible, setModalVisible, select
         onChangePid={onChangePid}
       ></ModalCustom>
     </View>
-  )
-}
+  );
+};
 
-export default ListCreatives
+export default ListCreatives;
